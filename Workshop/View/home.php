81eb8b21dd8f0a45c97263d740afa7d0814d28c9
LeonardoJRibeiro <leonardojr410@gmail.com>
2019-08-31 20:55:52 -0300

    teste

--- Workshop/View/home.php
+++ Workshop/View/home.php
@@ -4,6 +4,7 @@
     </head>
     <body>
         <h1>Formulario</h1>
+        teste 123
         <form name="form" method="post" action="cadastro">
             <input type="text" name="nome">
             <input type="submit">
